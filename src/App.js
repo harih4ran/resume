c19393b25bb53c41c7d4d980ec7df72ccec10775
harih4ran <hariharank.offl@gmail.com>
2022-10-03 15:57:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import Home from './components/Home';
-import {Routes,BrowserRouter as Router ,Route } from "react-router-dom";
+import React from 'react'
+import Home from './components/Home'
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom'
 // import Categories from './components/Categories';
-import PageNotFound from './components/PageNotFound';
+import PageNotFound from './components/PageNotFound'
 const App = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Home/>} />
-        {/* <Route path="/snippets/:components" element={<Categories/>} exact/> */}
-        <Route path="*" element={<PageNotFound/>}  />
-
+        <Route path="/" element={<Home />} />
+        {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
     </Router>
-
   )
 }
-export default App;
+export default App
--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,7 +5,7 @@ export default function PageNotFound() {
   return (
     <div>
       <Helmet>
-        <title>Optcoder | Not Found</title>
+        <title>Hariharan | Not Found</title>
       </Helmet>
       <h1 className="pt-72 text-center text-black">404 found</h1>
     </div>
